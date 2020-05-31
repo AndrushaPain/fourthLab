1fb3a88972a3a0fb3142b846b23ec62d5366b352
AndrushaPain <andreygoreff@yandex.ru>
2020-05-31 23:53:47 +0800

    Создал класс Particle, добавил туда поля и метод генерации частицы

--- fourthLab/Form1.cs
+++ fourthLab/Form1.cs
@@ -12,9 +12,20 @@ namespace fourthLab
 {
     public partial class Form1 : Form
     {
+        List<Particle> particles = new List<Particle>();
         public Form1()
         {
             InitializeComponent();
+            picDisplay.Image = new Bitmap(picDisplay.Width, picDisplay.Height);
+            for (var i = 0; i < 500; ++i)
+            {
+                var particle = Particle.Generate();
+                // переношу частицы в центр изображения
+                particle.X = picDisplay.Image.Width / 2;
+                particle.Y = picDisplay.Image.Height / 2;
+                // добавляю список
+                particles.Add(particle);
+            }
         }
     }
 }
---
+++ fourthLab/Particle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fourthLab
+{
+    class Particle
+    {
+        public int Radius; // радуис частицы
+        public float X; // X координата положения частицы в пространстве
+        public float Y; // Y координата положения частицы в пространстве
+        public float Direction; // направление движения
+        public float Speed; // скорость перемещения
+
+        public static Random rand = new Random();
+
+        // метод генерации частицы
+        public static Particle Generate()
+        {
+           return new Particle
+            {
+                Direction = rand.Next(360),
+                Speed = 1 + rand.Next(10),
+                Radius = 2 + rand.Next(10)
+            };
+        }
+    }
+}
