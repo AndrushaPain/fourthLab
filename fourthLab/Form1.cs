5023f1942bd888c983e20a5f56e338d8536bc744
AndrushaPain <andreygoreff@yandex.ru>
2020-06-09 16:38:43 +0800

    Revert "Добавил эмитер с регулированием его направления и разброса"
    This reverts commit 499d512d5754021143286bee864219fb6ce4aa06.

--- fourthLab/Form1.cs
+++ fourthLab/Form1.cs
@@ -12,27 +12,89 @@ namespace fourthLab
 {
     public partial class Form1 : Form
     {
-        // List<Particle> particles = new List<Particle>();
-        DirectionEmiter emiter;
+        List<Particle> particles = new List<Particle>();
         public Form1()
         {
             InitializeComponent();
             picDisplay.Image = new Bitmap(picDisplay.Width, picDisplay.Height);
-            emiter = new DirectionEmiter
-            {
-                ParticlesCount = 300,
-                Position = new Point(picDisplay.Width / 2, picDisplay.Height / 2)
-            };
+           
         }
 
         private void UpdateState()
         {
-            emiter.UpdateState();
+            
+            foreach (var particle in particles)
+            {
+                particle.Life -= 1;
+                if (particle.Life < 0)
+                {
+                    // восстанавливаю здоровье
+                    particle.Life = 20 + Particle.rand.Next(100);
+                    // пермещаю частицу в центр
+                    particle.X = MousePositionX;
+                    particle.Y = MousePositionY;
+                     particle.Direction = Particle.rand.Next(360);
+                    particle.Speed = 1 + Particle.rand.Next(10);
+                    particle.Radius = 2 + Particle.rand.Next(20);
+                }
+                else
+                {
+                    var directionInRadians = particle.Direction / 180 * Math.PI;
+                    particle.X += (float)(particle.Speed * Math.Cos(directionInRadians));
+                    particle.Y -= (float)(particle.Speed * Math.Sin(directionInRadians));
+                }
+            }
+             Random random = new Random();
+            for (var i = 0; i < 10; ++i)
+            {
+                if (particles.Count < 300) // пока частиц меньше 300 генерируем новые
+                {
+                    var particle = ParticleImage.Generate();
+                    int candy =random.Next(1,9);
+                    switch (candy) {
+                        case 1: particle.image = Properties.Resources.red;
+                           break;
+                        case 2:
+                            particle.image = Properties.Resources.green;
+                            break;
+                        case 3:
+                            particle.image = Properties.Resources.blue;
+                            break;
+                        case 4:
+                            particle.image = Properties.Resources.turquoise;
+                            break;
+                        case 5:
+                            particle.image = Properties.Resources.purple;
+                            break;
+                        case 6:
+                            particle.image = Properties.Resources.heart;
+                            break;
+                        case 7:
+                            particle.image = Properties.Resources.stick;
+                            break;
+                        case 8:
+                            particle.image = Properties.Resources.lolipop;
+                            break;
+                    }
+
+                 //   particle.image = Properties.Resources.red;
+                    particle.X = MousePositionX;
+                    particle.Y = MousePositionY;
+                    particles.Add(particle);
+                }
+                else
+                {
+                    break; // а если частиц уже 500 штук, то ничего не генерирую
+                }
+            }
         }
 
         private void Render(Graphics g)
         {
-            emiter.Render(g);
+            foreach (var particle in particles)
+            {
+                particle.Draw(g);
+            }
         }
 
         private void timer1_Tick(object sender, EventArgs e)
@@ -46,19 +108,12 @@ namespace fourthLab
             picDisplay.Invalidate();
         }
 
+        private int MousePositionX = 0;
+        private int MousePositionY = 0;
         private void picDisplay_MouseMove(object sender, MouseEventArgs e)
         {
-            
-        }
-
-        private void tbDirection_Scroll(object sender, EventArgs e)
-        {
-            emiter.Direction = tbDirection.Value;
-        }
-
-        private void tbSpread_Scroll(object sender, EventArgs e)
-        {
-            emiter.Spread = tbSpread.Value;
+            MousePositionX = e.X;
+            MousePositionY = e.Y;
         }
     }
 }
